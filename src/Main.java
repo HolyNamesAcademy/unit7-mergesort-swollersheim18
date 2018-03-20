211ef5c6f3ded866af5df8c24b5ca46a881c0589
Harsha Gotur <hgotur@live.com>
2018-03-19 19:01:02 -0700

    First attempt at student version. Includes functions to be implemented and tests.

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Use main to run and test the below functions
+    }
+
+    /**
+     * Returns arrayList with all elements in sorted order. This function uses selection sort to
+     * sort the arrayList.
+     * @param arrayList the ArrayList to be sorted. arrayList cannot contain duplicates
+     */
+    ArrayList<Integer> selectionSort(ArrayList<Integer> arrayList) {
+        throw new UnsupportedOperationException("SelectionSort() has not been implemented yet");
+    }
+
+    /**
+     * Returns the index that value is located in the arrayList. This function searches linearly in
+     * the arrayList to find the value.
+     * @param arrayList the ArrayList containing the list of values to search. arrayList cannot contain duplicates
+     * @param value the value we are looking for in the array list
+     */
+    int linearSearch(ArrayList<Integer> arrayList, int value) {
+        throw new UnsupportedOperationException("LinearSearch() has not been implemented yet");
+    }
+
+    /**
+     * Returns the index that value is located in the arrayList. This function uses binary search in
+     * the arrayList to find the value.
+     * @param arrayList the ArrayList containing the list of values to search. THIS ARRAYLIST MUST BE
+     *                  IN SORTED ORDER. arrayList cannot contain duplicates
+     * @param value the value we are looking for in the array list
+     */
+    int binarySearch(ArrayList<Integer> arrayList, int value) {
+        throw new UnsupportedOperationException("LinearSearch() has not been implemented yet");
+    }
+}
