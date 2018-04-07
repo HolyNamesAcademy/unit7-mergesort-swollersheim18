ac09358440caccf50259babdf7a82fcb5a4ee7ee
Harsha Gotur <hgotur@live.com>
2018-04-07 14:23:54 -0700

    Added merge and sort helper functions and tests for them

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,10 @@ public class Main {
 
     /**
      * Puts the elements in the arrayList in sorted order from smallest to greatest.
-     * This function uses SelectionSort to sort the arrayList.
+     * This function uses selection sort to sort the arrayList.
      * @param arrayList the ArrayList to be sorted. arrayList cannot contain duplicates
      */
     public static void selectionSort(ArrayList<Integer> arrayList) {
-        throw new UnsupportedOperationException("SelectionSort() has not been implemented yet");
-    }
-
-    /**
-     * Puts the elements in the arrayList in sorted order from smallest to greatest.
-     * This function uses MergeSort to sort the arrayList.
-     * @param arrayList the ArrayList to be sorted. arrayList cannot contain duplicates
-     */
-    public static void mergeSort(ArrayList<Integer> arrayList) {
         throw new UnsupportedOperationException("SelectionSort() has not been implemented yet");
     }
 
@@ -45,5 +36,39 @@ public class Main {
      */
     public static int binarySearch(ArrayList<Integer> arrayList, int value) {
         throw new UnsupportedOperationException("LinearSearch() has not been implemented yet");
+    }
+
+    /**
+     * Puts the elements in the arrayList in sorted order from smallest to greatest.
+     * This function uses MergeSort to sort the arrayList.
+     * @param arrayList the ArrayList to be sorted. arrayList cannot contain duplicates
+     */
+    public static void mergeSort(ArrayList<Integer> arrayList) {
+        throw new UnsupportedOperationException("mergeSort() has not been implemented yet");
+    }
+
+    /**
+     * This function sorts the portion of arrayList specified by the range [lo, hi). The range
+     * includes lo but excludes hi (arrayList[lo] is the first element in the range, but
+     * arrayList[hi] is the first element after the last element in the range).
+     * @param arrayList the ArrayList to be sorted.
+     * @param lo the index of the first element in the range
+     * @param hi the index of the last element in the range + 1.
+     */
+    public static void sort(ArrayList<Integer> arrayList, int lo, int hi) {
+        throw new UnsupportedOperationException("sort() has not been implemented yet");
+    }
+
+    /**
+     * This function merges two consecutive, sorted ranges in the arrayList into one sorted range. The ranges
+     * are specified as [lo, mid) and [mid, hi). Each range includes the first element, but excludes
+     * the last element (the same way as in sort()).
+     * @param arrayList the ArrayList to be sorted.
+     * @param lo the index of the first element in the first range
+     * @param mid the boundary point of the two ranges. arrayList[mid] is in the second range.
+     * @param hi the index of the last element in the second range + 1.
+     */
+    public static void merge(ArrayList<Integer> arrayList, int lo, int mid, int hi) {
+        throw new UnsupportedOperationException("merge() has not been implemented yet");
     }
 }
--- test/MainTest.java
+++ test/MainTest.java
@@ -26,21 +26,6 @@ public class MainTest extends Main {
     }
 
     @Test
-    public void mergeSort() throws Exception {
-        // Arrange
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        arrayList.addAll(Arrays.asList(3, 8, 14, 2, 5, 7, 9, 6, 1, 4, 12));
-
-        // Act
-        mergeSort(arrayList);
-
-        // Assert
-        ArrayList<Integer> expectedArrayList = new ArrayList<>();
-        expectedArrayList.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 14));
-        assertEquals(expectedArrayList, arrayList);
-    }
-
-    @Test
     public void linearSearch() throws Exception {
         // Arrange
         ArrayList<Integer> arrayList = new ArrayList<>();
@@ -66,4 +51,48 @@ public class MainTest extends Main {
         assertEquals(4, index);
     }
 
+    @Test
+    public void mergeSort() throws Exception {
+        // Arrange
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.addAll(Arrays.asList(3, 8, 14, 2, 5, 7, 9, 6, 1, 4, 12));
+
+        // Act
+        mergeSort(arrayList);
+
+        // Assert
+        ArrayList<Integer> expectedArrayList = new ArrayList<>();
+        expectedArrayList.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 14));
+        assertEquals(expectedArrayList, arrayList);
+    }
+
+    @Test
+    public void sort() throws Exception {
+        // Arrange
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.addAll(Arrays.asList(3, 8, 14, 2, 5, 7, 9, 6, 1, 4, 12));
+
+        // Act
+        sort(arrayList, 1, 5);
+
+        // Assert
+        ArrayList<Integer> expectedArrayList = new ArrayList<>();
+        expectedArrayList.addAll(Arrays.asList(3, 2, 5, 8, 14, 7, 9, 6, 1, 4, 12));
+        assertEquals(expectedArrayList, arrayList);
+    }
+
+    @Test
+    public void merge() throws Exception {
+        // Arrange
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.addAll(Arrays.asList(3, 8, 14, 2, 5, 7, 9, 6, 1, 4, 12));
+
+        // Act
+        merge(arrayList, 0, 3, 6);
+
+        // Assert
+        ArrayList<Integer> expectedArrayList = new ArrayList<>();
+        expectedArrayList.addAll(Arrays.asList(2, 3, 5, 7, 8, 14, 9, 6, 1, 4, 12));
+        assertEquals(expectedArrayList, arrayList);
+    }
 }
